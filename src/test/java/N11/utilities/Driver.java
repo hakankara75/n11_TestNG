942649e4ba03182b03b9e4f28f24b331eed26557
HakanKara75 <hakanbatirhan@gmail.com>
2023-09-08 23:05:22 +0300

    v

--- src/test/java/N11/utilities/Driver.java
+++ src/test/java/N11/utilities/Driver.java
@@ -8,8 +8,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-
-import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class Driver {
     /*
@@ -54,7 +53,7 @@ public class Driver {
             }
 
         }
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         return driver;
     }
@@ -90,7 +89,7 @@ default:
         }
 
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         }
 
         return driver;
